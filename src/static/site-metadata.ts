d7a0f42542924e868239e40e9bc7a30d0297d129
frhfeng <82604834+frhfeng@users.noreply.github.com>
2023-10-12 02:59:40 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: '冯瑞恒的运动轨迹',
-  siteUrl: 'https://sport.frh.life',
+  siteUrl: 'http://frhfeng.github.io/workouts_page/',
   logo: 'https://pic.imgdb.cn/item/6419d1b1a682492fcc0e5c1a.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
